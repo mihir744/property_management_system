b1897509bf97531415be3c4889ee9633c2cfca9b
whosthis74 <inglepratik126@gmail.com>
2026-02-24 20:46:21 +0530

    removing new models

--- core/models.py
+++ core/models.py
@@ -10,4 +10,4 @@ class PasswordResetRequest (models.Model):
     def __str__(self):
         return f"{self.user.username} - {self.created_at}"
 # Create your models here.
-#thinking to create new view for the site
+#removing old models 

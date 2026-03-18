69a0d6b0e191a722e37863bcdb43492bf12671b1
pratik <inglepratik126@gmail.com>
2026-03-18 12:18:33 +0530

    initial commit

--- shivtej_V_0/settings.py
+++ shivtej_V_0/settings.py
@@ -86,10 +86,11 @@ TEMPLATES = [
 # ------------------------------------------------
 # Database (Railway compatible)
 # ------------------------------------------------
+
 DATABASES = {
     "default": dj_database_url.config(
-        default="sqlite:///db.sqlite3",
-        conn_max_age=600
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 #local database

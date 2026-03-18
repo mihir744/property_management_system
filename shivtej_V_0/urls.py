e38305a6fc48c73387cf23af5cc82a7a7d667c11
pratik <inglepratik126@gmail.com>
2026-03-18 12:45:28 +0530

    initial commit

--- shivtej_V_0/urls.py
+++ shivtej_V_0/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path("account/", include("accounts.urls")),
 
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(
